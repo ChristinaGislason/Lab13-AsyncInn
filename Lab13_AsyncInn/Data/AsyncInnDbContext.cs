d102d7a02304e4fec2966da8bda5e6712ba6f2d2
Christina Gislason <christinamkcho@gmail.com>
2018-10-28 11:53:33 -0700

    added entity framework core and SQL database

---
+++ Lab13_AsyncInn/Data/AsyncInnDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab13_AsyncInn.Data
+{
+    public class AsyncInnDbContext : DbContext
+    {
+        public AsyncInnDbContext(DbContextOptions<AsyncInnDbContext> options) : base (options)
+        {
+
+        }
+
+    }
+}
