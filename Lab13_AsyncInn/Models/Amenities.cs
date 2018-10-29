342b238d6182d4b5ceae4be38459462740a25c04
Christina Gislason <christinamkcho@gmail.com>
2018-10-28 20:58:27 -0700

    seeded hotel, room, and amenities databases; added data annotations

--- Lab13_AsyncInn/Models/Amenities.cs
+++ Lab13_AsyncInn/Models/Amenities.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace Lab13_AsyncInn.Models
     public class Amenities
     {
         public int ID { get; set; }
+
+        [Display(Name = "Amenity")]
         public string Name { get; set; }
 
         public ICollection<RoomAmenities> RoomAmenities { get; set; }
